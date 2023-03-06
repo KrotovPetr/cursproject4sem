4ea9e39fa189bc578f395312bf192ef514776e1c
Krotov Petr <83280920+KrotovPetr@users.noreply.github.com>
2023-03-06 16:46:48 +0300

    Add README.md

--- Client/src/Components/Header/Bar/Bar.tsx
+++ Client/src/Components/Header/Bar/Bar.tsx
@@ -1,7 +1,7 @@
 import React, { useState } from 'react';
 import styles from './bar.module.scss';
 import Logo from '../../../Utils/Images/logo2.png';
-import {Link, NavLink} from "react-router-dom";
+import {NavLink} from "react-router-dom";
 const Bar = () => {
     const [activePoint, setActive] = useState<number>(0);
     return (
@@ -11,51 +11,51 @@ const Bar = () => {
                 className={
                     activePoint === 0 ? styles.activePoint : styles.point
                 }
-                onClick={(): void => {
-                    setActive(0);
-                }}
+
             >
-                <NavLink to="/tools" className={styles.link}>Tools</NavLink>
+                <NavLink to="/tools" className={styles.link} onClick={(): void => {
+                    setActive(0);
+                }}>Tools</NavLink>
             </div>
             <div
                 className={
                     activePoint === 1 ? styles.activePoint : styles.point
                 }
-                onClick={(): void => {
-                    setActive(1);
-                }}
+
             >
-                <NavLink to="/supplies" className={styles.link}>Supplies</NavLink>
+                <NavLink to="/supplies" className={styles.link} onClick={(): void => {
+                    setActive(1);
+                }}>Supplies</NavLink>
             </div>
             <div
                 className={
                     activePoint === 2 ? styles.activePoint : styles.point
                 }
-                onClick={(): void => {
-                    setActive(2);
-                }}
+
             >
-                <NavLink to="/service" className={styles.link}>Service</NavLink>
+                <NavLink to="/service" className={styles.link}  onClick={(): void => {
+                    setActive(2);
+                }}>Service</NavLink>
             </div>
             <div
                 className={
                     activePoint === 3 ? styles.activePoint : styles.point
                 }
-                onClick={(): void => {
-                    setActive(3);
-                }}
+
             >
-                <NavLink to="/rent" className={styles.link}>Rent</NavLink>
+                <NavLink to="/rent" className={styles.link} onClick={(): void => {
+                    setActive(3);
+                }}>Rent</NavLink>
             </div>
             <div
                 className={
                     activePoint === 4 ? styles.activePoint : styles.point
                 }
-                onClick={(): void => {
-                    setActive(4);
-                }}
+
             >
-                <NavLink to="/" className={styles.link}>Our deals</NavLink>
+                <NavLink to="/" className={styles.link} onClick={(): void => {
+                    setActive(4);
+                }}>Our deals</NavLink>
             </div>
         </div>
     );
---
+++ README.md
@@ -0,0 +1,37 @@
+<div style="display: flex; flex-flow: column nowrap; justify-content: center; align-items: flex-start">
+<img src="./Client/src/Utils/Images/logo.png" alt="logo" style="align-self: center">
+<h1 style="align-self: center">"Samodelkin Group"</h1>
+<p style="text-align: center">
+Проект клиент-серверного приложения "Строительный магазин". Данный проект является курсовой работой, 
+использовался в начальном виде в 4 семестре, был полностью переделан в 6 семестре.
+</p>
+
+<h3>Клиентская часть</h3>
+<div>
+    <img src="https://img.shields.io/badge/TypeScript-blue?logo=TypeScript&logoColor=white&style=for-the-badge" alt="TypeScript Badge"/>
+    <img src="https://img.shields.io/badge/JavaScript-yellow?logo=JavaScript&logoColor=white&style=for-the-badge" alt="JavaScript Badge"/>
+    <img src="https://img.shields.io/badge/HTML5-orange?logo=HTML5&logoColor=white&style=for-the-badge" alt="HTML5 Badge"/>
+    <img src="https://img.shields.io/badge/SСSS-grey?logo=SASS&logoColor=white&style=for-the-badge" alt="SASS Badge"/>
+    <img src="https://img.shields.io/badge/React-blue?logo=React&logoColor=white&style=for-the-badge" alt="React Badge"/>
+    <img src="https://img.shields.io/badge/Redux-blueviolet?logo=Redux&logoColor=white&style=for-the-badge" alt="Redux Badge"/>
+    <img src="https://img.shields.io/badge/Cypress-9cf?logo=Cypress&logoColor=white&style=for-the-badge" alt="Cypress Badge"/> 
+    <img src="https://img.shields.io/badge/Jest-blue?logo=Jest&logoColor=white&style=for-the-badge" alt="Jest Badge"/>
+    <img src="https://img.shields.io/badge/Docker-blue?logo=Docker&logoColor=white&style=for-the-badge" alt="Docker Badge"/>
+</div>
+<h3>Серверная часть</h3>
+<div>
+    <img src="https://img.shields.io/badge/Express-white?logo=Express&logoColor=black&style=for-the-badge" alt="Express Badge"/>
+    <img src="https://img.shields.io/badge/Node.JS-grey?logo=Node.JS&logoColor=white&style=for-the-badge" alt="Node Badge"/>
+    <img src="https://img.shields.io/badge/TypeScript-blue?logo=TypeScript&logoColor=white&style=for-the-badge" alt="TypeScript Badge"/>
+    <img src="https://img.shields.io/badge/JavaScript-yellow?logo=JavaScript&logoColor=white&style=for-the-badge" alt="JavaScript Badge"/>
+    <img src="https://img.shields.io/badge/Docker-blue?logo=Docker&logoColor=white&style=for-the-badge" alt="Docker Badge"/>
+</div>
+<h3>База данных</h3>
+<div>
+<img src="https://img.shields.io/badge/PostgreSQl-blue?logo=PostgreSQl&logoColor=white&style=for-the-badge" alt="PostgreSQL Badge"/>
+<img src="https://img.shields.io/badge/Sequelize-gr?logo=Sequelize&logoColor=white&style=for-the-badge" alt="Sequelize Badge"/>
+</div>
+
+
+
+</div>
